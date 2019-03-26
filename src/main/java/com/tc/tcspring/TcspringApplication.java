146932c0228a52dcc8f4b4e0743b176b2625cc43
cjtclinsan <woshicjtc123@163.com>
2019-03-26 09:22:24 +0800

    新建spring

---
+++ src/main/java/com/tc/tcspring/TcspringApplication.java
@@ -0,0 +1,13 @@
+package com.tc.tcspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcspringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tc/tcspring/TcspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tc.tcspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TcspringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
